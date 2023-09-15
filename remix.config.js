915c486c2c7147f921bdbbd1422b823d485bdb83
BlakeIsMyWaifu <33188727+BlakeIsMyWaifu@users.noreply.github.com>
2023-09-15 20:52:00 +0100

    🚑 Fix vercel build

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,11 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
+  publicPath: "/build/",
+  serverBuildPath: "api/index.js",
+  serverMainFields: ["main", "module"],
+  serverMinify: false,
   serverModuleFormat: "cjs",
+  serverPlatform: "node",
   postcss: false
 };
